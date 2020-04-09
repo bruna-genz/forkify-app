ca1120b248de30e5b3cc4eb54bbecf676b8ffe84
Bruna <brunagenz92@gmail.com>
2020-04-09 14:27:22 -0300

    create List class

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import { elements, renderLoader, clearLoader } from './views/base';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
@@ -115,3 +116,9 @@ import * as recipeView from './views/recipeView';
 
       console.log(state.recipe);
   })
+
+  /**
+  * RECIPE CONTROLLER
+  */
+
+  window.l = new List();
